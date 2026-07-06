cbef65c31f35cfb756f231f5e16af326954aadf0
PLP <peilipingplp@gmail.com>
2026-07-06 13:21:02 +0800

    u

--- db/Headers.lua
+++ db/Headers.lua
@@ -9,16 +9,19 @@ local H = {
         NumberCol.new("end_time", "number", 0, false),
         NumberCol.new("crc32", "number", 0, false),
     },
+    KV = {},
     MAGIC = 2026070100,
     header_length = 0,
     header_format = "",
     crc_format = "",
 }
 
-for _, col in pairs(H.ES) do
+for _, col in ipairs(H.ES) do
+    H.KV[col.name] = col
     H.header_length = H.header_length + col.size
     H.header_format = H.header_format .. col.format
 end
-H.crc_format = H.ES[4].format .. H.ES[5].format
+
+H.crc_format = H.KV.start_time.format .. H.KV.end_time.format
 
 return H
