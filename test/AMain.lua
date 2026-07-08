11c765864125ae4ae8b48e4f4ed6b4368e5c699b
PLP <peilipingplp@gmail.com>
2026-07-08 14:25:52 +0800

    u

--- test/AMain.lua
+++ test/AMain.lua
@@ -1,14 +1,16 @@
-local t_Functions = require("test.t_Functions")
-
-local test_cases = { t_Functions }
+local test_cases = {
+    { require("test.t_Functions") },
+    { require("test.t_RingBuffer") },
+}
 
 for i, tc in ipairs(test_cases) do
-    print("Running " .. i .. "...")
-    for k, fc in pairs(tc) do
+    print("Testing : " .. tc[2] .. "...")
+    for k, fc in pairs(tc[1]) do
         if type(fc) == "function" and string.sub(k, 1, 4) == "test" then
-            print("===== " .. k .. " =====")
+            print("test method : " .. k)
             fc()
-            print("Passed.")
+            print("test result : Passed.")
         end
     end
+    print("--------------------------------------------------------------------------------")
 end
