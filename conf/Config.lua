dd335d49cf30d58e00d1ea7dab3e3a3ea19b48cb
PLP <peilipingplp@gmail.com>
2026-07-09 20:38:20 +0800

    u

--- conf/Config.lua
+++ conf/Config.lua
@@ -3,6 +3,8 @@ local S = {}
 local TABLE_CONFIG = {
     columns = nil,
     block_size = nil,
+    rollupExpr = nil,
+    parallelExpr = nil,
 }
 
 TABLE_CONFIG.__index = TABLE_CONFIG
@@ -11,6 +13,8 @@ function TABLE_CONFIG.new(columns, block_size)
     local self = setmetatable({}, TABLE_CONFIG)
     self.columns = columns
     self.block_size = (block_size or 4 * 1024 * 1024)
+    self.rollupExpr = "first(open),max(high),min(low),last(close),sum(volume),sum(quote_volume),sum(taker_buy_volume),sum(taker_buy_quote_volume),sum(count),last(long_short_delta),last(open_interest)"
+    self.parallelExpr = "last(close),lr(close),last(open_interest),lr(open_interest)"
     return self
 end
 
@@ -33,7 +37,6 @@ local function create_kline_base(interval_sec)
 end
 
 local function create_kline_ln(interval_sec)
-
     local kline_ln_columns = {
         { name = "time", type = "timestamp", interval = interval_sec },
         { name = "close", type = "number", precision = 2, signed = false },
@@ -46,16 +49,14 @@ local function create_kline_ln(interval_sec)
     return TABLE_CONFIG.new(kline_ln_columns)
 end
 
-S.BTC_KL_5M = create_kline_base(300)
+S.BTC_KL_5M  = create_kline_base(300)
 S.BTC_KL_15M = create_kline_base(900)
-S.BTC_KL_1H = create_kline_base(3600)
-S.BTC_KL_4H = create_kline_base(14400)
+S.BTC_KL_1H  = create_kline_base(3600)
+S.BTC_KL_4H  = create_kline_base(14400)
 
-S.BTC_LN_5M = create_kline_ln(300)
+S.BTC_LN_5M  = create_kline_ln(300)
 S.BTC_LN_15M = create_kline_ln(900)
-S.BTC_LN_1H = create_kline_ln(3600)
-S.BTC_LN_4H = create_kline_ln(14400)
+S.BTC_LN_1H  = create_kline_ln(3600)
+S.BTC_LN_4H  = create_kline_ln(14400)
 
---rollupExpr = "first(open),max(high),min(low),last(close),sum(volume),sum(quote_volume),sum(taker_buy_volume),sum(taker_buy_quote_volume),sum(count),last(long_short_delta),last(open_interest)"
---parallelExpr = "tail(close),lrUP(close),lrDN(close),tail(open_interest),lrUP(open_interest),lrDN(open_interest)"
 return S
