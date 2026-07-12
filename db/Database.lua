fd7ee2f736684aadfa4ca652f3c51d7e976790b4
PLP <peilipingplp@gmail.com>
2026-07-12 22:08:28 +0800

    u

--- db/Database.lua
+++ db/Database.lua
@@ -19,7 +19,7 @@ function DB.new(root_path, v_table_name, safe)
     for table_name, config in pairs(Config) do
         if not v_table_name or v_table_name == table_name then
             local path = get_file_path(self.root_path, table_name)
-            if safe then
+            if not safe and safe then
                 self.data_tables[table_name] = DataTable.safe_new(table_name, config, path)
             else
                 self.data_tables[table_name] = DataTable.new(table_name, config, path)
